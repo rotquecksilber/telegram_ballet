140a29fb062c9206fd77f1ecf22487540c07f3aa
Valentin <prefectzao@gmail.com>
2026-03-03 15:57:51 +0400

    21

--- ballet-backend/src/main.ts
+++ ballet-backend/src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import helmet from 'helmet';
+//import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(helmet());
+  //app.use(helmet());
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
