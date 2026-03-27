ec03b998814af16bf95332f1ab51399cf0abe9ef
Valentin <prefectzao@gmail.com>
2026-03-27 15:50:01 +0400

    styles

--- vite-project/src/pages/Profile.tsx
+++ vite-project/src/pages/Profile.tsx
@@ -482,6 +482,7 @@ export const Profile = ({ onRegisterSuccess }: ProfileProps) => {
                         padding: '12px',
                         border: 'none',
                         marginTop: '15px',
+                        marginBottom: '15px',
                         borderRadius: '12px',
                         fontWeight: '600',
                         background: 'linear-gradient(45deg, #FFD700, #FFA500)',
